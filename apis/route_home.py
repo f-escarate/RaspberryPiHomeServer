bb364b004b33ccb7abc40f1ea58422c1b4a5ba44
f-escarate <felipeescaratefernandez@gmail.com>
2025-04-04 00:33:42 -0300

    fix: open steamlink without pressing Enter

--- apis/route_home.py
+++ apis/route_home.py
@@ -4,21 +4,18 @@ import os
 from fastapi.responses import HTMLResponse
 
 TEMPLATES_PATH = os.getenv("TEMPLATES_PATH", "./templates")
+template: str = open(os.path.join(TEMPLATES_PATH, "index.html"), "r").read()
 
 home_router = APIRouter()
 
 @home_router.get("/")
 async def get_user():
-    template: str = open(os.path.join(TEMPLATES_PATH, "index.html"), "r").read()
-    print(template)
     return HTMLResponse(template, status_code=200)
 
 @home_router.get("/openSteam")
 async def open_steam():
     # Opens the steamlink process
-    print("aaaa")
-    res = os.system("steamlink")
+    res = os.system("steamlink -y")
     if res != 0:
         raise HTTPException(status_code=500, detail="Failed to open Steam Link")
-    print("bbbb")
     return {"message": "Steam is open"}
